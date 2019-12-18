ded6218d73ed163439f51a9348d5c2119f2b0249
Hongli Xu <wu402a8zc7@gmail.com>
2019-12-18 16:15:34 -0500

    add basic react components

---
+++ app/javascript/components/Home/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {         
+      course_modules: [
+      { id: 1, title: "1. Setting up project", description: "lorem ipsum", active: false },
+      { id: 2, title: "2. Add react to the project", description: "lorem ipsum", active: false },
+      { id: 3, title: "3. Building a hello world", description: "lorem ipsum", active: false },
+      { id: 4, title: "4. Add react router", description: "lorem ipsum", active: false }
+    ] };
+  }
+  render() {
+    return (
+      <div>This is the home componenet</div>
+    );
+  }
+}
+
+export default Home;
