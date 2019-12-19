4538b783d2e011456e67b2dfe67681f7c8b17b84
Hongli Xu <wu402a8zc7@gmail.com>
2019-12-18 20:16:51 -0500

    use axios to fetch data from rails backend

--- app/javascript/components/Home/Home.js
+++ app/javascript/components/Home/Home.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react'
 import Jombotron from './Jombotron'
 import Table from './Table/Table';
+import axios from 'axios'
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {         
       course_modules: [
-      { id: 1, title: "1. Setting up project", description: "lorem ipsum", active: false },
-      { id: 2, title: "2. Add react to the project", description: "lorem ipsum", active: false },
-      { id: 3, title: "3. Building a hello world", description: "lorem ipsum", active: false },
-      { id: 4, title: "4. Add react router", description: "lorem ipsum", active: false }
+        
     ] };
+  }
+
+  componentDidMount(){
+    axios.get('/episodes.json')
+    .then(data=>{
+      let res = []
+      data.data.data.map((data)=>{
+        res.push({id: data.id, title: data.title, description: data.description, active: false})
+
+        this.setState({course_modules: res});
+      })
+    }).catch(data => {
+
+    })
   }
 
   handleVideoChange = (item, event)=>{
