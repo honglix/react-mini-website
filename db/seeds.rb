3e014f9b7482a72a5cf97441d4cb62f1649e79ba
Hongli Xu <wu402a8zc7@gmail.com>
2019-12-18 20:08:30 -0500

    add rails models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+course = Course.create(title:"Hello World", description: "Create a React App with Rails")
+
+section = Section.create(title:"Chapter 1", description: "First Chapter", course: course)
+
+episode = Episode.create([
+    {title: "1. Setting up project", description: "lorem ipsum", url: "https://www.youtube.com/watch?v=5F_JUvPq410", section: section},
+    {title: "2. Add react to the project", description: "lorem ipsum", url: "https://www.youtube.com/watch?v=5F_JUvPq410", section: section },
+    {title: "3. Building a hello world", description: "lorem ipsum", url:"https://www.youtube.com/watch?v=5F_JUvPq410", section: section},
+    {title: "4. Add react router", description: "lorem ipsum", url:"https://www.youtube.com/watch?v=5F_JUvPq410", section: section }
+])
